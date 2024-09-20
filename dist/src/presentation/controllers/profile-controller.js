1b0f63eda608ff8a4ac6d20d0e210c6d40cf3358
CaioAugustobrg <caioaugustobrg@gmail.com>
2024-09-20 02:46:00 -0300

    home laptop

--- dist/src/presentation/controllers/profile-controller.js
+++ dist/src/presentation/controllers/profile-controller.js
@@ -5,16 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ProfileController = void 0;
 const get_profile_ids_by_user_1 = require("../../application/usecases/get-profile-ids-by-user");
+const logger_1 = __importDefault(require("../../utils/logger"));
 const csv_writer_1 = require("csv-writer");
 const fs_1 = __importDefault(require("fs"));
 const browser_controller_1 = require("./browser-controller");
-const puppeteer_1 = __importDefault(require("puppeteer"));
+const puppeteer_extra_1 = __importDefault(require("puppeteer-extra"));
+const puppeteer_extra_plugin_stealth_1 = __importDefault(require("puppeteer-extra-plugin-stealth"));
 const verify_bot_status_1 = require("../../application/usecases/verify-bot-status");
 const open_browser_1 = require("../../application/usecases/open-browser");
 const promises_1 = require("node:timers/promises");
 const close_browser_1 = require("../../application/usecases/close-browser");
 const child_process_1 = require("child_process");
 const path_1 = __importDefault(require("path"));
+puppeteer_extra_1.default.use((0, puppeteer_extra_plugin_stealth_1.default)());
 function ensureCsvFileExists(filePath) {
     if (!fs_1.default.existsSync(filePath)) {
         fs_1.default.writeFileSync(filePath, '', 'utf8');
@@ -37,9 +40,7 @@ function clearCsvFile(filePath) {
     console.log(`Conteúdo do arquivo ${filePath} apagado.`);
 }
 function openCsvFile(filePath) {
-    // Ajuste o caminho do arquivo conforme necessário
     const absolutePath = path_1.default.resolve(filePath);
-    // Comando para abrir o arquivo
     (0, child_process_1.exec)(`start "" "${absolutePath}"`, (error) => {
         if (error) {
             console.error('Erro ao abrir o arquivo:', error);
@@ -59,7 +60,7 @@ class ProfileController {
         const targetPage = 'https://x.com/messages';
         const csvFilePath = 'not_running_profiles.csv';
         let notRunningProfiles = [];
-        let runningProfiles = []; // Array para perfis que funcionaram
+        let runningProfiles = [];
         ensureCsvFileExists(csvFilePath);
         clearCsvFile(csvFilePath);
         while (true) {
@@ -68,15 +69,13 @@ class ProfileController {
                     try {
                         logWithColor(`Running: ${userId}`, 'blue');
                         const puppeteerUrl = await this.openBrowserAndGetUrl(userId);
-                        const browser = await puppeteer_1.default.connect({
+                        const browser = await puppeteer_extra_1.default.connect({
                             browserWSEndpoint: puppeteerUrl,
                             defaultViewport: null
                         });
-                        // Verificação de perfis
                         const initialResult = await this.verifyProfiles({ profileId: userId, url: puppeteerUrl }, userIds);
                         if (initialResult) {
                             notRunningProfiles.push(...initialResult.notRunningProfilesAfterVerification);
-                            // Se não tiver perfis não rodando, adiciona ao array de perfis que funcionaram
                             if (initialResult.notRunningProfilesAfterVerification.length === 0) {
                                 runningProfiles.push({ profileId: userId, url: puppeteerUrl });
                                 logWithColor(`Profile working: ${userId}`, 'green', true);
@@ -91,69 +90,88 @@ class ProfileController {
                     }
                     catch (error) {
                         console.error(`Error processing user ID ${userId}: ${error.message}`);
+                        logger_1.default.error(`Error processing user ID ${userId}: ${error.stack}`);
                     }
                 }
-                // Salva todos os perfis não rodando no CSV
                 await this.saveToCsv(notRunningProfiles, csvFilePath);
-                logWithColor(`Those who don't have the Cupid Bot on have been added to: ${csvFilePath}`, 'yellow');
-                logWithColor(`last: ${notRunningProfiles.length} browsers are not working`, 'red', true);
-                // Abrindo perfis que funcionaram
                 for (const profile of runningProfiles) {
                     logWithColor(`Opening working profile: ${profile.profileId}`, 'green');
-                    await this.verifyProfiles(profile, userIds); // Reutiliza verifyProfiles
+                    await this.verifyProfiles(profile, userIds);
                 }
-                // Abrindo perfis que não funcionaram
                 for (const profile of notRunningProfiles) {
                     logWithColor(`Opening not running profile: ${profile.profileId}`, 'red');
-                    await this.verifyProfiles(profile, userIds); // Reutiliza verifyProfiles
+                    await this.verifyProfiles(profile, userIds);
                 }
+                logWithColor(`Those who don't have the Cupid Bot on have been added to: ${csvFilePath}`, 'yellow');
+                logWithColor(`last: ${notRunningProfiles.length} browsers are not working`, 'red', true);
                 notRunningProfiles = [];
-                runningProfiles = []; // Reseta o array de perfis que funcionaram
+                runningProfiles = [];
                 console.log('Waiting 1 minute before the next profile check...');
                 await (0, promises_1.setTimeout)(60000);
             }
             catch (error) {
-                return;
+                console.error('Error in findAllProfiles:', error.message);
+                logger_1.default.error('Error in findAllProfiles:', error.stack);
+                return; // Exit loop on error
             }
         }
     }
     async openBrowserAndGetUrl(userId) {
-        const data = await this.browserController.OpenBrowser(userId);
-        //      console.log('OpenBrowser response data:', data.data.ws.puppeteer);
-        return data.data.ws.puppeteer;
+        try {
+            const data = await this.browserController.OpenBrowser(userId);
+            console.log('OpenBrowser response data:', data); // Log the entire response
+            if (!data || !data.data || !data.data.ws || !data.data.ws.puppeteer) {
+                throw new Error(`Invalid response structure for user ID ${userId}`);
+            }
+            return data.data.ws.puppeteer;
+        }
+        catch (error) {
+            console.error(`Error opening browser for user ID ${userId}: ${error.message}`);
+            logger_1.default.error(`Error opening browser for user ID ${userId}: ${error.stack}`);
+            throw error; // Re-throw the error for handling
+        }
     }
     async handleProfilePages(browser, targetPage, userId, notRunningProfiles) {
+        const twitterUrl = "https://x";
+        const adsPowerUrl = "https://start";
         try {
             const pages = await browser.pages();
+            for (let page of pages) {
+                if (!page.url().startsWith(twitterUrl) || !page.url().startsWith(adsPowerUrl)) {
+                    page.close();
+                }
+            }
             const pageURL = await browser.newPage();
             await pageURL.bringToFront();
             await pageURL.goto(targetPage, { waitUntil: 'networkidle2' });
             let verifyCupidBot = new verify_bot_status_1.PageHandler(pageURL);
             const response = await verifyCupidBot.handlePage(targetPage, userId);
-            //      console.log('Verification response:', response);
             if (response !== 'running ok') {
                 notRunningProfiles.push({ profileId: response, url: browser.wsEndpoint() });
             }
-            //   console.log('Page URL, we are home', pageURL.url());
-            // await pageURL.close();
         }
         catch (error) {
-            // Adiciona o perfil ao array mesmo em caso de erro
             notRunningProfiles.push({ profileId: userId, url: browser.wsEndpoint() });
-            //   console.error(`Error handling profile pages for user ${userId}:`, error);
+            console.error(`Error handling profile pages for user ${userId}: ${error.message}`);
+            logger_1.default.error(`Error handling profile pages for user ${userId}: ${error.stack}`);
         }
     }
     async verifyProfiles(profileInfo, ids) {
-        //  logWithColor(`User ID: ${ids}`, 'blue');
         let notRunningProfilesAfterVerification = [];
-        // Verifica apenas o perfil informado em profileInfo
-        const puppeteerUrl = await this.openBrowserAndGetUrl(profileInfo.profileId);
-        const browser = await puppeteer_1.default.connect({
-            browserWSEndpoint: puppeteerUrl,
-            defaultViewport: null
-        });
-        await this.handleProfilePages(browser, 'https://x.com/messages', profileInfo.profileId, notRunningProfilesAfterVerification);
-        return { message: 'Verificação de perfil concluída com sucesso', notRunningProfilesAfterVerification };
+        try {
+            const puppeteerUrl = await this.openBrowserAndGetUrl(profileInfo.profileId);
+            const browser = await puppeteer_extra_1.default.connect({
+                browserWSEndpoint: puppeteerUrl,
+                defaultViewport: null
+            });
+            await this.handleProfilePages(browser, 'https://x.com/messages', profileInfo.profileId, notRunningProfilesAfterVerification);
+            return { message: 'Verificação de perfil concluída com sucesso', notRunningProfilesAfterVerification };
+        }
+        catch (error) {
+            console.error(`Error verifying profiles for user ID ${profileInfo.profileId}: ${error.message}`);
+            logger_1.default.error(`Error verifying profiles for user ID ${profileInfo.profileId}: ${error.stack}`);
+            return { notRunningProfilesAfterVerification };
+        }
     }
     async saveToCsv(notRunningProfiles, filePath) {
         const csvWriter = (0, csv_writer_1.createObjectCsvWriter)({
@@ -161,19 +179,15 @@ class ProfileController {
             header: [
                 { id: 'profileId', title: 'Profile ID' }
             ],
-            append: true // Adiciona os dados ao arquivo existente
+            append: true
         });
-        // Formata a data e a hora atual
-        const currentDateTime = new Date().toISOString().replace('T', ' ').split('.')[0]; // Formato YYYY-MM-DD HH:MM:SS
-        // Grava a data e a hora como uma linha separada
+        const currentDateTime = new Date().toISOString().replace('T', ' ').split('.')[0];
         fs_1.default.appendFileSync(filePath, `${currentDateTime}\n`);
-        // Prepara os registros para incluir apenas o profileId
         const records = notRunningProfiles.map(profile => ({
             profileId: profile.profileId
         }));
         await csvWriter.writeRecords(records);
         openCsvFile('not_running_profiles.csv');
-        //  console.log('Data saved to CSV file:', filePath);
     }
 }
 exports.ProfileController = ProfileController;
@@ -185,7 +199,8 @@ async function callFindAllProfiles() {
         await userProfile.findAllProfiles(userIds.userIds);
     }
     catch (error) {
-        // console.error('Error calling findAllProfiles:', error);
+        console.error('Error calling findAllProfiles:', error.message);
+        logger_1.default.error('Error calling findAllProfiles:', error.stack);
     }
     finally {
         getProfilesIdsByUser.close();
--- src/presentation/controllers/profile-controller.ts
+++ src/presentation/controllers/profile-controller.ts
@@ -7,20 +7,28 @@ import ApiError from "../../utils/api-error";
 import { NotRunningProfile } from "../../domain/types/profile";
 import { Request, Response } from "express";
 import { BrowserController } from "./browser-controller";
-import puppeteer, { Browser } from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import { Browser, executablePath } from "puppeteer";
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 import { PageHandler } from "../../application/usecases/verify-bot-status";
 import { OpenBrowserUseCase } from "../../application/usecases/open-browser";
-import {setTimeout} from 'node:timers/promises'
+import { setTimeout } from 'node:timers/promises';
 import { CloseBrowserUseCase } from "../../application/usecases/close-browser";
 import csvParser from "csv-parser";
 import { exec } from "child_process";
 import path from "path";
+
+
+puppeteer.use(StealthPlugin())
+
+
 function ensureCsvFileExists(filePath: string) {
     if (!fs.existsSync(filePath)) {
         fs.writeFileSync(filePath, '', 'utf8');
         console.log(`Arquivo ${filePath} criado.`);
     }
 }
+
 function logWithColor(message: string, color: string, bold: boolean = false) {
     const colors: { [key: string]: string } = {
         red: '\x1b[31m',
@@ -38,19 +46,18 @@ function clearCsvFile(filePath: string) {
     fs.writeFileSync(filePath, '', 'utf8');
     console.log(`Conteúdo do arquivo ${filePath} apagado.`);
 }
+
 function openCsvFile(filePath: string) {
-    // Ajuste o caminho do arquivo conforme necessário
     const absolutePath = path.resolve(filePath);
     
-    // Comando para abrir o arquivo
     exec(`start "" "${absolutePath}"`, (error) => {
         if (error) {
             console.error('Erro ao abrir o arquivo:', error);
         }
     });
 }
+
 export class ProfileController {
-    
     browserController: BrowserController;
 
     constructor() {
@@ -75,7 +82,7 @@ export class ProfileController {
         const csvFilePath = 'not_running_profiles.csv';
     
         let notRunningProfiles: NotRunningProfile[] = [];
-        let runningProfiles: NotRunningProfile[] = []; // Array para perfis que funcionaram
+        let runningProfiles: NotRunningProfile[] = [];
     
         ensureCsvFileExists(csvFilePath);
         clearCsvFile(csvFilePath);
@@ -89,15 +96,12 @@ export class ProfileController {
                         const puppeteerUrl = await this.openBrowserAndGetUrl(userId);
                         const browser = await puppeteer.connect({
                             browserWSEndpoint: puppeteerUrl,
-                            defaultViewport: null
-                        });
+                            defaultViewport: null                        });
     
-                        // Verificação de perfis
                         const initialResult = await this.verifyProfiles({ profileId: userId, url: puppeteerUrl }, userIds);
                         if (initialResult) {
                             notRunningProfiles.push(...initialResult.notRunningProfilesAfterVerification);
     
-                            // Se não tiver perfis não rodando, adiciona ao array de perfis que funcionaram
                             if (initialResult.notRunningProfilesAfterVerification.length === 0) {
                                 runningProfiles.push({ profileId: userId, url: puppeteerUrl });
                                 logWithColor(`Profile working: ${userId}`, 'green', true);
@@ -110,50 +114,61 @@ export class ProfileController {
     
                     } catch (error: any) {
                         console.error(`Error processing user ID ${userId}: ${error.message}`);
+                        logger.error(`Error processing user ID ${userId}: ${error.stack}`);
                     }
                 }
     
-                // Salva todos os perfis não rodando no CSV
                 await this.saveToCsv(notRunningProfiles, csvFilePath);
-                logWithColor(`Those who don't have the Cupid Bot on have been added to: ${csvFilePath}`, 'yellow');
-                logWithColor(`last: ${notRunningProfiles.length} browsers are not working`, 'red', true);
-                // Abrindo perfis que funcionaram
                 for (const profile of runningProfiles) {
                     logWithColor(`Opening working profile: ${profile.profileId}`, 'green');
-                    await this.verifyProfiles(profile, userIds); // Reutiliza verifyProfiles
+                    await this.verifyProfiles(profile, userIds);
                 }
-    
-                // Abrindo perfis que não funcionaram
+                
                 for (const profile of notRunningProfiles) {
                     logWithColor(`Opening not running profile: ${profile.profileId}`, 'red');
-                    await this.verifyProfiles(profile, userIds); // Reutiliza verifyProfiles
+                    await this.verifyProfiles(profile, userIds);
                 }
+                logWithColor(`Those who don't have the Cupid Bot on have been added to: ${csvFilePath}`, 'yellow');
+                logWithColor(`last: ${notRunningProfiles.length} browsers are not working`, 'red', true);
     
                 notRunningProfiles = []; 
-                runningProfiles = []; // Reseta o array de perfis que funcionaram
+                runningProfiles = []; 
     
                 console.log('Waiting 1 minute before the next profile check...');
                 await setTimeout(60000); 
     
             } catch (error: any) {
-                return;
+                console.error('Error in findAllProfiles:', error.message);
+                logger.error('Error in findAllProfiles:', error.stack);
+                return; // Exit loop on error
             }
         }
     }
-    
-    
-
-
-
     private async openBrowserAndGetUrl(userId: string): Promise<string> {
-        const data = await this.browserController.OpenBrowser(userId);
-  //      console.log('OpenBrowser response data:', data.data.ws.puppeteer);
-        return data.data.ws.puppeteer;
+        try {
+            const data = await this.browserController.OpenBrowser(userId);
+            console.log('OpenBrowser response data:', data); // Log the entire response
+            if (!data || !data.data || !data.data.ws || !data.data.ws.puppeteer) {
+                throw new Error(`Invalid response structure for user ID ${userId}`);
+            }
+            return data.data.ws.puppeteer;
+        } catch (error: any) {
+            console.error(`Error opening browser for user ID ${userId}: ${error.message}`);
+            logger.error(`Error opening browser for user ID ${userId}: ${error.stack}`);
+            throw error; // Re-throw the error for handling
+        }
     }
-
+    
     private async handleProfilePages(browser: Browser, targetPage: string, userId: string, notRunningProfiles: NotRunningProfile[]) {
+        const twitterUrl = "https://x"
+        const adsPowerUrl = "https://start"
         try {
             const pages = await browser.pages();
+            for (let page of pages) {
+                if (!page.url().startsWith(twitterUrl) || !page.url().startsWith(adsPowerUrl)) {
+                    page.close()
+                }
+            }
             const pageURL = await browser.newPage();
             await pageURL.bringToFront();
             await pageURL.goto(targetPage, { waitUntil: 'networkidle2' });
@@ -161,33 +176,34 @@ export class ProfileController {
             let verifyCupidBot = new PageHandler(pageURL);
             const response = await verifyCupidBot.handlePage(targetPage, userId);
     
-      //      console.log('Verification response:', response);
             if (response !== 'running ok') {
                 notRunningProfiles.push({ profileId: response, url: browser.wsEndpoint() });
             }
-    
-         //   console.log('Page URL, we are home', pageURL.url());
-            // await pageURL.close();
-        } catch (error) {
-            // Adiciona o perfil ao array mesmo em caso de erro
-            notRunningProfiles.push({ profileId: userId, url: browser.wsEndpoint() }); 
-         //   console.error(`Error handling profile pages for user ${userId}:`, error);
+        } catch (error: any) {
+            notRunningProfiles.push({ profileId: userId, url: browser.wsEndpoint() });
+            console.error(`Error handling profile pages for user ${userId}: ${error.message}`);
+            logger.error(`Error handling profile pages for user ${userId}: ${error.stack}`);
         }
     }
+
     async verifyProfiles(profileInfo: NotRunningProfile, ids: string[]) {
-      //  logWithColor(`User ID: ${ids}`, 'blue');
         let notRunningProfilesAfterVerification: NotRunningProfile[] = [];
+
+        try {
+            const puppeteerUrl = await this.openBrowserAndGetUrl(profileInfo.profileId);
+            const browser = await puppeteer.connect({
+                browserWSEndpoint: puppeteerUrl,
+                defaultViewport: null
+            });
+            
+            await this.handleProfilePages(browser, 'https://x.com/messages', profileInfo.profileId, notRunningProfilesAfterVerification);
     
-        // Verifica apenas o perfil informado em profileInfo
-        const puppeteerUrl = await this.openBrowserAndGetUrl(profileInfo.profileId);
-        const browser = await puppeteer.connect({
-            browserWSEndpoint: puppeteerUrl,
-            defaultViewport: null
-        });
-        
-        await this.handleProfilePages(browser, 'https://x.com/messages', profileInfo.profileId, notRunningProfilesAfterVerification);
-    
-        return { message: 'Verificação de perfil concluída com sucesso', notRunningProfilesAfterVerification };
+            return { message: 'Verificação de perfil concluída com sucesso', notRunningProfilesAfterVerification };
+        } catch (error: any) {
+            console.error(`Error verifying profiles for user ID ${profileInfo.profileId}: ${error.message}`);
+            logger.error(`Error verifying profiles for user ID ${profileInfo.profileId}: ${error.stack}`);
+            return { notRunningProfilesAfterVerification };
+        }
     }
     
     private async saveToCsv(notRunningProfiles: NotRunningProfile[], filePath: string) {
@@ -196,27 +212,19 @@ export class ProfileController {
             header: [
                 { id: 'profileId', title: 'Profile ID' }
             ],
-            append: true // Adiciona os dados ao arquivo existente
+            append: true
         });
     
-        // Formata a data e a hora atual
-        const currentDateTime = new Date().toISOString().replace('T', ' ').split('.')[0]; // Formato YYYY-MM-DD HH:MM:SS
-    
-        // Grava a data e a hora como uma linha separada
+        const currentDateTime = new Date().toISOString().replace('T', ' ').split('.')[0];
         fs.appendFileSync(filePath, `${currentDateTime}\n`);
     
-        // Prepara os registros para incluir apenas o profileId
         const records = notRunningProfiles.map(profile => ({
             profileId: profile.profileId
         }));
     
         await csvWriter.writeRecords(records);
         openCsvFile('not_running_profiles.csv');
-        
-      //  console.log('Data saved to CSV file:', filePath);
-      
     }
-    
 }     
 
 async function callFindAllProfiles() {
@@ -226,9 +234,9 @@ async function callFindAllProfiles() {
     try {
         const userIds = await getProfilesIdsByUser.handle();
         await userProfile.findAllProfiles(userIds.userIds);
-      
-    } catch (error) {
-       // console.error('Error calling findAllProfiles:', error);
+    } catch (error: any) {
+        console.error('Error calling findAllProfiles:', error.message);
+        logger.error('Error calling findAllProfiles:', error.stack);
     } finally {
         getProfilesIdsByUser.close();
     }
